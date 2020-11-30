2f290fb8412bc04013abf693febb95eb41605fe7
Robert Tratseuski <smalz4282@gmail.com>
2020-11-29 19:34:40 -0500

    Initial Commit

---
+++ Smart Expence Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Expence Tracker
+//
+//  Created by Robert Tratseuski on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
